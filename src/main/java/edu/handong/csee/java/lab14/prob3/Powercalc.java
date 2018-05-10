90d3aa5648792690ec5e0d1694a69957b38060f2
Choiwoolim <woolim4301@gmail.com>
2018-05-10 10:09:50 +0900

    Prob3 single comment line.

--- src/main/java/edu/handong/csee/java/lab14/prob3/Powercalc.java
+++ src/main/java/edu/handong/csee/java/lab14/prob3/Powercalc.java
@@ -1,17 +1,17 @@
-package edu.handong.csee.java.lab14.prob3;
+package edu.handong.csee.java.lab14.prob3;				//package name
 
-public class Powercalc {
-	public long power(int n, int p) throws Exception{
-		long result = 0;
+public class Powercalc {								//class name is Powercalc
+	public long power(int n, int p) throws Exception{	//Create the public long type power method. it makes exception
+		long result = 0;								//Declare the long type variable.
 		
-		result = (long)Math.pow(n, p);
-		if(n<0 || p<0) {
-			throw new Exception("n or p should not be negative.");
+		result = (long)Math.pow(n, p);					//result is calculated n^p
+		if(n<0 || p<0) {								//n is less than 0 or p is less than 0
+			throw new Exception("n or p should not be negative.");	//create the new exception and store.
 		}
-		if(n==0 && p ==0) {
-			throw new Exception("n and p should not be zero.");
+		if(n==0 && p ==0) {								//n is 0 and p is zero
+			throw new Exception("n and p should not be zero.");		//create the new exception and store.
 		}
-		return result;
+		return result;									//return result.
 	}
 
 }
--- src/main/java/edu/handong/csee/java/lab14/prob3/Prob3.java
+++ src/main/java/edu/handong/csee/java/lab14/prob3/Prob3.java
@@ -1,19 +1,19 @@
-package edu.handong.csee.java.lab14.prob3;
-import java.util.Scanner;
+package edu.handong.csee.java.lab14.prob3;								//package name
+import java.util.Scanner;												//using scanner
 
-public class Prob3 {
-	public static final Powercalc myCalculator = new Powercalc();
-	public static final Scanner in = new Scanner(System.in);
+public class Prob3 {													//main class name is prob3
+	public static final Powercalc myCalculator = new Powercalc();		//instantiate the powercalc class.
+	public static final Scanner in = new Scanner(System.in);			//using scanner
 
-	public static void main(String[] args) {
-		while(in.hasNextInt()) {
-			int n = in.nextInt();
-			int p = in.nextInt();
+	public static void main(String[] args) {							//call main method
+		while(in.hasNextInt()) {										//Loop
+			int n = in.nextInt();										//assign. n is input integer value.
+			int p = in.nextInt();										//assign. p is input integer value.
 
-			try {
-				System.out.println(myCalculator.power(n, p));
-			} catch(Exception e) {
-				System.out.println(e);
+			try {														//find the exception process.
+				System.out.println(myCalculator.power(n, p));			//print the n's power p
+			} catch(Exception e) {										//if exception is happen.
+				System.out.println(e);									//print the error message.
 			}
 		}
 	}
