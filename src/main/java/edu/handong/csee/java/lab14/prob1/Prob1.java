378a0d823c3a8d5b9b603e604ec05f97f61ec0c6
Choiwoolim <woolim4301@gmail.com>
2018-05-10 09:40:44 +0900

    Prob1 single line comment.

--- src/main/java/edu/handong/csee/java/lab14/prob1/Prob1.java
+++ src/main/java/edu/handong/csee/java/lab14/prob1/Prob1.java
@@ -1,25 +1,25 @@
-package edu.handong.csee.java.lab14.prob1;
-import java.util.Scanner;
-import java.util.Random;
+package edu.handong.csee.java.lab14.prob1;			//Package name
+import java.util.Scanner;							//Using Scanner
+import java.util.Random;							//Using Random function
 
-public class Prob1 {
+public class Prob1 {								//main Class name is Prob1
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner keyboard = new Scanner(System.in);
-		Random rand = new Random();
-		int limit = 0;
-		int speed = 0;
+	public static void main(String[] args) {			//Call main method
 		
-		System.out.println("Set the speed limit, officer : ");
-		limit = keyboard.nextInt();
-		speed = rand.nextInt(101);
+		Scanner keyboard = new Scanner(System.in);		//Call Scanner
+		Random rand = new Random();						//Declare the random function
+		int limit = 0;									//Declare the integer variable limit
+		int speed = 0;									//Declare the integer variable speed
 		
-		SpeedLimitter lim = new SpeedLimitter(limit, speed);
+		System.out.println("Set the speed limit, officer : ");		//Print the following sentence
+		limit = keyboard.nextInt();						//limit is input value.
+		speed = rand.nextInt(101);						//speed is random value.
 		
-		lim.warnSpeedLimit();
+		SpeedLimitter lim = new SpeedLimitter(limit, speed);	//Instantiate the Speedlimitter and assign limit and speed.
 		
-		keyboard.close();
+		lim.warnSpeedLimit();							//Call warnSpeedLimit method
+		
+		keyboard.close();								//close Scanner
 
 	}
 
--- src/main/java/edu/handong/csee/java/lab14/prob1/SpeedLimitter.java
+++ src/main/java/edu/handong/csee/java/lab14/prob1/SpeedLimitter.java
@@ -1,25 +1,25 @@
-package edu.handong.csee.java.lab14.prob1;
+package edu.handong.csee.java.lab14.prob1;					//Package name
 
-public class SpeedLimitter {
-	private int speed = 0;
-	private int limit = 0;
+public class SpeedLimitter {							//class name is SpeedLimitter
+	private int speed = 0;								//Declare the private integer variable speed
+	private int limit = 0;								//Declare the private integer variable limit
 
-	public SpeedLimitter(int limit, int speed) {
-		this.limit = limit;
-		this.speed = speed;
+	public SpeedLimitter(int limit, int speed) {		//Create the SpeedLimitter method.
+		this.limit = limit;								//assign the limit value.
+		this.speed = speed;								//assign the speed value.
 	}
 
-	public void warnSpeedLimit() {
-		try {
-			if(this.speed>this.limit) {
-				throw new Exception("Speed Limit " + this.limit + "km exceeded");
+	public void warnSpeedLimit() {						//Create the void type warnSpeedLimit method
+		try {											//Find a Exception.
+			if(this.speed>this.limit) {					//speed is 
+				throw new Exception("Speed Limit " + this.limit + "km exceeded");		//add the new Exception's message
 			}
-			System.out.println("You are a low abiding citizen! ");
-		}catch(Exception e) {
-			System.out.println(e.getMessage());
-			System.out.println("You are being fined. ");
+			System.out.println("You are a low abiding citizen! ");		//print the following sentence.
+		}catch(Exception e) {											//Exception happen!!
+			System.out.println(e.getMessage());							//print the exception message
+			System.out.println("You are being fined. ");				//print the following sentence.
 		}
-		System.out.println("You're current speed : " + this.speed);
+		System.out.println("You're current speed : " + this.speed);		//Print the following sentence.
 	}
 
 
