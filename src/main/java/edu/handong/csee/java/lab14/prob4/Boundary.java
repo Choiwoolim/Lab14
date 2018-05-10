b6e6b201027844bfce9961275127f93799f6489f
Choiwoolim <woolim4301@gmail.com>
2018-05-10 10:17:50 +0900

    Prob4 single line comment

--- src/main/java/edu/handong/csee/java/lab14/prob4/Boundary.java
+++ src/main/java/edu/handong/csee/java/lab14/prob4/Boundary.java
@@ -1,13 +1,13 @@
-package edu.handong.csee.java.lab14.prob4;
+package edu.handong.csee.java.lab14.prob4;				//package name
 
-public class Boundary {
-	private static int [] arr = new int[5];
-	private static int cnt = 0;
-	public void receive(int num) throws ArrayIndexOutOfBoundsException{
-		int i = cnt;
-		arr[i] = num;
-		cnt++;
-		System.out.printf("arr[%d] <- %d \n", i, num);
+public class Boundary {									//class name is boundary
+	private static int [] arr = new int[5];				//declare the integer array. and assign 
+	private static int cnt = 0;							//declare the private static integer variable cnt.
+	public void receive(int num) throws ArrayIndexOutOfBoundsException{		//create the public void type method. it makes exception
+		int i = cnt;									// declare the integer variable i and assign i
+		arr[i] = num;									//arr[i] is assigned the num value.
+		cnt++;											//cnt = cnt+1
+		System.out.printf("arr[%d] <- %d \n", i, num);	//print the following sentence.
 	}
 
 }
--- src/main/java/edu/handong/csee/java/lab14/prob4/Prob4.java
+++ src/main/java/edu/handong/csee/java/lab14/prob4/Prob4.java
@@ -1,20 +1,20 @@
-package edu.handong.csee.java.lab14.prob4;
-import java.util.Scanner;
+package edu.handong.csee.java.lab14.prob4;			//package name
+import java.util.Scanner;							//using scanner
 
-public class Prob4 {
-	public static void main(String[] args) {
-		Scanner keyboard = new Scanner(System.in);
-		int num = 0;
-		Boundary arr = new Boundary();
-		boolean repeat = true;
-		while(repeat) {
-			try {
-				System.out.print("Enter an integer: ");
-				num = keyboard.nextInt();
-				arr.receive(num);
-			}catch(ArrayIndexOutOfBoundsException e){
-				System.out.println("Invalid array index access!");
-				repeat = false;
+public class Prob4 {								//main class name is prob4
+	public static void main(String[] args) {		//call main method
+		Scanner keyboard = new Scanner(System.in);	//Declare the keyboard scanner.
+		int num = 0;								//declare the integer variable num.
+		Boundary arr = new Boundary();				//instantiate the boundary class.
+		boolean repeat = true;						//declare the boolean type variable and assign true.
+		while(repeat) {								//Loop
+			try {									//find the exception process
+				System.out.print("Enter an integer: ");		//print the following sentence.
+				num = keyboard.nextInt();			// assign num is input value.
+				arr.receive(num);					//call receive method.
+			}catch(ArrayIndexOutOfBoundsException e){	//if exception is happen
+				System.out.println("Invalid array index access!");	//print the following sentence.
+				repeat = false;						//boolea repeat is false.(exit the loop)
 
 			}
 		}
